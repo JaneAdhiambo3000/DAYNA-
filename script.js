8ee145f8bed12f7bb9ae18f004d0106a06058458
Moringa School Access <shebaz3000@gmail.com>
2020-08-22 16:08:33 +0300

    added a footer

---
+++ script.js
@@ -0,0 +1,6 @@
+function male(date){
+    if (date%7===0) console.log("Kwasi");
+    else if(date%6===0) console.log("Hey! you were born on Monday, your Akan name is Kwadwo.");
+    else(console.log("Hey! you were born on Wednesday, your Akan name is Kwabena."));
+}
+alert(male(1));
