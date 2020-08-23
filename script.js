8d18f772d07abdd6de06907bd4fb4d935c225be3
Moringa School Access <shebaz3000@gmail.com>
2020-08-23 12:34:13 +0300

    modified script js

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ function getAkanName(){
     var myGender = document.getElementsByName("gender");
     var dateOfBirth = new Date(myBirthday);
     var dayOfTheWeek = dateOfBirth.getDay();
+     if(myBirthday === ""){
+        document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Didn't Submit a Valid Date!</div>";
+        $('#message').addClass("animated shake");
+    }
 }
+
+
    
